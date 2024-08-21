ea544cb5a0e3845c242e3fdd317019654f7f8f9a
navivan123 <ivanfran49er@gmail.com>
2024-08-20 22:25:50 -0400

    I should've commited a long time ago.  Added Asteroids, Player, and Shot and relevant constants.  REPL loop updating groups now.

---
+++ asteroid.py
@@ -0,0 +1,16 @@
+from player import CircleShape
+import pygame
+import random
+from constants import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        #self.position = pygame.Vector2(x, y)
+        #self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
-from constants import *
+import sys
+from constants     import *
+from player        import Player
+from asteroid      import Asteroid
+from asteroidfield import AsteroidField
+from shot          import Shot
 
 def main():
     print("Starting asteroids!")
@@ -9,12 +14,43 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+
+    updatable = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots     = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidf = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for thing in updatable:
+            thing.update(dt)
+        
         screen.fill("black")
+        for thing in drawable:
+            thing.draw(screen)
+        
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
+
+        for thing in asteroids:
+            if thing.collides(player):
+                print("Game over!")
+                sys.exit()
 
 
 if __name__ == "__main__":
